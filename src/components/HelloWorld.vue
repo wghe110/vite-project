0fdaa031f6cd800b3c09781078862f1378ff804a
wanggonghe <397856638@qq.com>
2021-11-26 15:59:00 +0800

    fix(eslint):修复eslint配置报错

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
-  
+
 <template>
   <h1>{{ msg }}</h1>
 
---
+++ src/components/demo.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div>
+        test
+    </div>
+</template>
+<script>
+export default {
+  setup() {
+    const a = 1;
+    console.log("a");
+    debugger;
+
+    return {
+      a,
+    };
+  },
+};
+</script>
+<style lang="">
+</style>
