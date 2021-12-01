d63f16c506ed6647f825938aef9fd687f63e66ac
wanggonghe <397856638@qq.com>
2021-12-01 17:34:54 +0800

    fix(init)

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,22 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: './',
+  plugins: [vue()],
+  resolve: {
+    // 别名
+    alias: []
+  },
+  css: {
+    // 指定传递给 CSS 预处理器的选项
+    preprocessorOptions: {
+      scss: {
+        additionalData: `$injectedColor: orange;`
+      }
+    }
+  },
+  server: {
+    // 反向代理
+    proxy: {}
+  },
 })
