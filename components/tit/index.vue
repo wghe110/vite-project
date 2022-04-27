e23d092e27a2967c82a97fd02277bc395b8b608a
wanggonghe <wanggonghe@cestc.cn>
2022-04-27 14:46:54 +0800

    [A]:sass

---
+++ components/tit/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2 class="tit">
+      <slot>--</slot>
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.tit {
+  color: blue;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
 <template>
-  <div>vue2-js-project</div>
+  <div class="demo">vue2-js-project</div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    demoFn() {
+      const a = 1;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.demo {
+  color: red;
+}
+</style>
