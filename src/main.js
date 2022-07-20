9652e6ebb138dc6219e6697fd04063518176674d
wanggonghe <wanggonghe@cestc.cn>
2022-07-20 16:02:16 +0800

    [A]:添加pinia

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
+const pinia = createPinia()
 
 // normalize.css
 import 'normalize.css'
 
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
