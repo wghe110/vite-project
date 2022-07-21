0bdb4124d571213ea190984838b7da8df55b2133
wanggonghe <wanggonghe@cestc.cn>
2022-07-21 10:05:23 +0800

    [A]:global-components

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export default {
+  install: (app, options) => {
+    /**
+     * 1. 通过 app.component() 和 app.directive() 注册一到多个全局组件或自定义指令。
+     * 2. 通过 app.provide() 使一个资源可被注入进整个应用。
+     * 3. 向 app.config.globalProperties 中添加一些全局实例属性或方法
+     */
+  }
+}
--- src/main.js
+++ src/main.js
@@ -7,8 +7,12 @@ const pinia = createPinia()
 // normalize.css
 import 'normalize.css'
 
+// 全局组件/指令/属性或方法
+import globalPlugins from './components/index'
+
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
+app.use(globalPlugins)
 
 app.mount('#app')
