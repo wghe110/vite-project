41aa68d0b7d7113171d9299326b647214bb96260
wanggonghe <wanggonghe@cestc.cn>
2022-09-27 09:56:35 +0800

    [A]:添加全屏提示

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,42 @@
 <template>
   <router-view></router-view>
+
+  <div class="tips" v-if="!isFullScreen">
+    <p>是否要全屏？</p>
+    <button @click="yes">是</button>
+    <button @click="no">否</button>
+  </div>
 </template>
 
-<script>
+<script setup>
 import mitt from 'mitt'
+import { provide, ref } from 'vue';
+import full from './untils/fullscreen'
 
-import { provide } from 'vue';
+const emitter = mitt();
+const isFullScreen = ref(false)
+isFullScreen.value = full.isFullScreen()
 
-export default {
-  setup() {
-    const emitter = mitt();
-    // 下发mitt
-    provide('emitter', emitter)
-  }
+// 下发mitt
+provide('emitter', emitter)
+
+const yes = () => {
+  full.fullscreen(document.documentElement)
+  isFullScreen.value = true;
 }
+const no = () => {
+  isFullScreen.value = true;
+}
+
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.tips {
+  position: fixed;
+  background-color: #fff;
+  padding: 40px;
+  left: 50%;
+  top: 0;
+  transform: translateX(-50%);
+}
 </style>
