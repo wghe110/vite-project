19e9ff9a58e3c4f3d0a1b8442679fe118a1e57aa
wanggonghe <397856638@qq.com>
2023-10-22 21:57:55 +0800

    [M]:demo

---
+++ src/views/demo/amap/InfoWindow/Custer.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-tooltip content="Top center" placement="top">
+    <el-button>Dark</el-button>
+  </el-tooltip>
+</template>
+
+<script>
+import { Button, Tooltip } from "element-ui";
+
+export default {
+  components: {
+    ElButton: Button,
+    ElTooltip: Tooltip,
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/demo/amap/InfoWindow/index.js
+++ src/views/demo/amap/InfoWindow/index.js
@@ -1,26 +1,17 @@
 import Vue from "vue/dist/vue.esm.js";
-import ContentVue from './Content.vue'
-
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-
-Vue.use(ElementUI);
+import ContentVue from './Custer.vue'
 
 const createInfoWindow = (AMap) => {
-  // 挂载vue元素
-  const element = document.createElement("div");
-
   const app = new Vue({
     components: { ContentVue },
     template: `<ContentVue/>`,
-  });
-  app.$mount(element);
+  }).$mount();
 
 
   // 创建信息窗体
   const infoWindow = new AMap.InfoWindow({
     anchor: "bottom-center",
-    content: element,
+    content: app.$el,
   });
 
   return { infoWindow, app };
